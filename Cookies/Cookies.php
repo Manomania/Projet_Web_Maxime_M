7cb916edf9833a307d976c8a84d25868d7223190
DESKTOP-LUTTRJ6\Mano <maxime.martinez96@hotmail.fr>
2018-02-03 12:03:27 +0100

    Test des cookies pour plus tard

---
+++ Cookies/Cookies.php
@@ -0,0 +1,9 @@
+<?php
+
+setcookie('Prénom', '', time() + 24*3600, null, null, false, true);
+setcookie('Nom', '', time() + 24*3600, null, null, false, true);
+setcookie('Email', '', time() + 24*3600, null, null, false, true);
+setcookie('Age', '', time() + 24*3600, null, null, false, true);
+setcookie('Sexe', '', time() + 24*3600, null, null, false, true);
+
+?>
