819fe7076e393c87d28047ac65bafc3a194a03f0
DESKTOP-LUTTRJ6\Mano <maxime.martinez96@hotmail.fr>
2018-02-03 12:03:15 +0100

    Header + Footer en php

--- Modules/Header.php
+++ Modules/Header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="style.css" />
-    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
-    <link rel="shortcut icon" href="Images/Favicon.ico" />
-    <title>Klipay - Accueil</title>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+        <link rel="shortcut icon" href="Images/Favicon.ico" />
+        <title>Klipay - Accueil</title>
+    </head>
